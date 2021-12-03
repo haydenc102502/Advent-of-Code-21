a158585007d240f001fa6a7e520e74d27d4d8ffe
haydenc102502 <hc6285@g.rit.edu>
2021-12-02 19:09:03 -0500

    Advent of Code 21  Day 01 Part 01

--- day01_1.py
+++ day01_1.py
@@ -1,15 +1,22 @@
 def find_inc(filename):
-    counter = 0
+    counter = 1
+    a_list = []
+
     with open(filename) as file:
-        for lines in range(len(file) - 1):
-            if file[lines] < file[lines + 1]:
+        for line in file:
+            line.strip()
+            a_list.append(int(line))
+        for i in range(len(a_list)):
+            if i == 0:
+                continue
+            if a_list[i-1] < a_list[i]:
                 counter += 1
-    return counter
+        return counter
 
 
 def main():
 
-    print(find_inc("day01_1.txt"))
+    print(find_inc("data/day01_1.txt"))
 
 if __name__ == "__main__":
     main()
