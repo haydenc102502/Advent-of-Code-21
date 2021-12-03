7d819a3f3e31772449d674ea3272e631cd16e72f
haydenc102502 <hc6285@g.rit.edu>
2021-12-02 20:16:53 -0500

    Advent of Code Day 02 Part 02 ****

--- day02.py
+++ day02.py
@@ -2,11 +2,7 @@ def position(filename):
     with open(filename) as file:
         x = 0
         y = 0
-        """
-        forward 5
-        """
-        x = 0
-        y = 0
+        aim = 0
         for line in file:
             if line[0] == "f":
                 x += int(line[len(line) - 2])
@@ -16,11 +12,26 @@ def position(filename):
                 y -= int(line[len(line) - 2])
         return x * y
 
+def position_with_depth(filename):
+    with open(filename) as file:
+        x = 0
+        y = 0
+        aim = 0
+        for line in file:
+            if line[0] == "d":
+                aim += int(line[len(line) - 2])
+            elif line[0] == "u":
+                aim -= int(line[len(line) - 2])
+            else:
+                x += int(line[len(line) - 2])
+                y += aim * int(line[len(line) - 2])
+        return x * y
+
 
 
 def main():
 
-    print(position("data/day02.txt"))
+    print(position_with_depth("data/day02.txt"))
 
 if __name__ == "__main__":
     main()
